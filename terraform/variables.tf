a78c9a9bd92a9fefaa00fcc84ddce1adc6f8876a
gpdurga06-collab <gpdurga06@gmail.com>
2026-04-04 17:26:46 -0500

    adding Main.tf and variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-2"
+  description = "AWS region where all resources will be created"
+}
+
+
+
+variable "project_name" {
+  type        = string
+  default     = "ecommerce-data-platform"
+  description = "Name of the project used to name all resources"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Deployment environment - dev or prod"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "Your AWS account ID"
+}
