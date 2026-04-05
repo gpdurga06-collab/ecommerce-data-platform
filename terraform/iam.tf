48dad571a74142550308f36c7045b06e9b83b10a
gpdurga06-collab <gpdurga06@gmail.com>
2026-04-05 15:50:06 -0400

    added Iam roles for glue, lambda, emr, step_functions

---
+++ terraform/iam.tf
@@ -0,0 +1,147 @@
+
+#glue role
+# The "aws_iam_role" resource creates an IAM role that AWS Glue can assume to perform its operations.
+
+resource "aws_iam_role" "glue_role" {
+  name = "${var.project_name}-${var.environment}-glue-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-glue-role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "glue_role_policy_attachment" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_role_policy_attachment_s3" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+#lambda role
+# The "aws_iam_role" resource creates an IAM role that AWS Lambda can assume to perform its operations.
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.environment}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-lambda-role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment_s3" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+#EMR role
+# The "aws_iam_role" resource creates an IAM role that Amazon EMR can assume
+
+resource "aws_iam_role" "emr_role" {
+  name = "${var.project_name}-${var.environment}-emr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "elasticmapreduce.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-emr-role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "emr_role_policy_attachment" {
+  role       = aws_iam_role.emr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "emr_role_policy_attachment_s3" {
+  role       = aws_iam_role.emr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+#step function role
+# The "aws_iam_role" resource creates an IAM role that AWS Step Functions can assume
+
+resource "aws_iam_role" "step_function_role" {
+  name = "${var.project_name}-${var.environment}-step-function-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-step-function-role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "step_function_role_policy_attachment" {
+  role       = aws_iam_role.step_function_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "step_function_role_policy_attachment_s3" {
+  role       = aws_iam_role.step_function_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
