b5ad38fa3d9905d1982296ed2ae2454d8dc041be
gpdurga06-collab <gpdurga06@gmail.com>
2026-04-06 19:24:06 -0400

    EMR clsuter with true-option edited

--- terraform/emr.tf
+++ terraform/emr.tf
@@ -9,7 +9,7 @@ resource "aws_emr_cluster" "emr_cluster" {
   service_role  = aws_iam_role.emr_role.arn
 
   termination_protection            = false
-  keep_job_flow_alive_when_no_steps = false
+  keep_job_flow_alive_when_no_steps = true
 
   ec2_attributes {
     instance_profile = aws_iam_instance_profile.emr_instance_profile.arn
